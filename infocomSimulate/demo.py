d34cb77b922b0209ea8dbbca7c35067d98fe94ce
chenwuji <53996386@qq.com>
2016-07-02 14:27:17 +0800

    开始infocom实验

--- infocomSimulate/demo.py
+++ infocomSimulate/demo.py
@@ -10,9 +10,8 @@ import tools
 if __name__ == '__main__':
     rd.initRoadData()
     print rd.getRoadLen('1099','1157')  #计算道路长度
-    print tools.calculate(120.640319889,31.2916944701,120.639999, 31.29039)#  计算距离
+    print tools.calculate(120.640319889,31.2916944701,120.639999, 31.29039)   #  计算距离
     print rd.getRoadPointLocation('1099')  #获取路口位置经纬度
     print rd.getNeighbourList('1099')  #获取相邻的路口
     print rd.judgeBounds('1099')  #判断是否超出边界
-
 
